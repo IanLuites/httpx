2dc8856ffb45e7dbb5e541e2cb0e09a942d84ad0
Ian Luites <IanLu@eu.square-enix.com>
2017-07-22 02:57:21 +0100

    Add basic response struct.

---
+++ lib/httpx/response.ex
@@ -0,0 +1,21 @@
+defmodule HTTPX.Response do
+  @moduledoc ~S"""
+  HTTP response.
+  """
+
+  @typedoc ~S"""
+  HTTP response.
+  """
+  @type t :: %__MODULE__{
+    status: pos_integer,
+    headers: [{String.t, String.t}],
+    body: binary | map | list,
+  }
+
+  @enforce_keys [:status]
+  defstruct [
+    :status,
+    headers: [],
+    body: "",
+  ]
+end
