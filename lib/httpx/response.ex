2a7529fcf53487abc1545916600c0f539d15ec14
Ian Luites <ian@luites.com>
2022-06-22 14:09:51 +0200

    🏷️ fix type for body when JSON decoded

--- lib/httpx/response.ex
+++ lib/httpx/response.ex
@@ -9,7 +9,7 @@ defmodule HTTPX.Response do
   @type t :: %__MODULE__{
           status: pos_integer,
           headers: [{String.t(), String.t()}],
-          body: binary | map | list
+          body: binary | map | list | number | boolean | nil
         }
 
   @enforce_keys [:status]
